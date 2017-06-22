46cb96cdc41dc99b41990d79109be9bae4f54216
chris in manifold <365940729@qq.com>
2017-06-22 21:39:36 +0800

    modified start up bash

--- m100_rcphone.sh
+++ m100_rcphone.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-source /home/ubuntu/M100_RCphone/devel/setup.bash &&
-sleep 5
-source /home/ubuntu/M100_RCphone/src/Onboard-SDK-ROS/rcphone.sh &
+source /home/ubuntu/M100_RCphone/src/Onboard-SDK-ROS/rcphone.sh&
 sleep 20
 rosrun dji_sdk_demo dji_sdk_rctest1
--- rcphone.sh
+++ rcphone.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-roscore &
-sleep 5
-roslaunch rosbridge_server rosbridge_websocket.launch &
+source /home/ubuntu/M100_RCphone/devel/setup.bash
+sleep 1
+roscore&
+sleep 12
+roslaunch rosbridge_server rosbridge_websocket.launch&
 sleep 5
 roslaunch dji_sdk sdk_manifold.launch
