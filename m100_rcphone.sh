6a1e8822b5c8bd99a6b1616d13bbf217d2caf630
chris in manifold <365940729@qq.com>
2017-06-26 09:51:43 +0800

    bash change

--- m100_rcphone.sh
+++ m100_rcphone.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source /home/ubuntu/M100_RCphone/devel/setup.bash
+sleep 1
 source /home/ubuntu/M100_RCphone/src/Onboard-SDK-ROS/rcphone.sh&
 sleep 20
 rosrun dji_sdk_demo dji_sdk_rctest1
